0f0c93eaf7e3714541dd94021bb91ee3a021a97e
ajay-0019 <ajay63679948@gmail.com>
2025-07-06 19:42:57 +0530

    Added Ajay Kumar's Currency Converter project

---
+++ src/mentees/ajaymeena/index.jsx
@@ -0,0 +1,10 @@
+import App from './App';
+
+export const meta = {
+  title: "Currency Converter",
+  author: "Ajay Kumar Meena",
+  description: "A simple React-based currency converter app that allows users to convert between different currencies. ",
+  thumbnail: "/assets/thumbnails/currency-converter.jpeg" 
+};
+
+export default App;
